aeec0d7218691f5383b840657540d3a02786cc2e
= <ray.bartnikowski@gmail.com>
2022-06-05 20:32:28 -0400

    Adding a database view for filtering out the comment report segments

---
+++ database/migrations/2022_06_06_001928_create_comments_report_view.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared(
+            'CREATE VIEW `laravel`.`comments_report` AS
+            SELECT 
+                `laravel`.`sweetwater_test`.`orderid` AS `orderid`,
+                `laravel`.`sweetwater_test`.`comments` AS `comments`,
+                `laravel`.`sweetwater_test`.`shipdate_expected` AS `shipdate_expected`,
+                (CASE
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%candy%\') THEN \'candy\'
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%call me%\') THEN \'callme\'
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%refer%\') THEN \'referral\'
+                    WHEN
+                        ((LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%sign%\')
+                            OR (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%signature%\'))
+                    THEN
+                        \'signature\'
+                    ELSE \'misc\'
+                END) AS `comment_type`
+            FROM
+                `laravel`.`sweetwater_test`'
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP VIEW `laravel`.`comments_report`;');
+    }
+};
