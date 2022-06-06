8640232b201607031ff960db5710f3934a299d43
= <ray.bartnikowski@gmail.com>
2022-06-06 01:51:45 -0400

    Further refining the view criteria, this time for referrals

---
+++ database/migrations/2022_06_06_054658_update_comments_report_view_referral.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared(
+            'DROP VIEW IF EXISTS `comments_report`;'
+        );
+
+        DB::unprepared(
+            'CREATE VIEW `laravel`.`comments_report` AS
+            SELECT 
+                `laravel`.`sweetwater_test`.`orderid` AS `orderid`,
+                `laravel`.`sweetwater_test`.`comments` AS `comments`,
+                `laravel`.`sweetwater_test`.`shipdate_expected` AS `shipdate_expected`,
+                (CASE
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) REGEXP \'(candy|smarties|bit o honey)\') THEN \'candy\'
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) REGEXP \'(call|contact me)\') THEN \'callme\'
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) REGEXP \'(referred|referral|told me about)\') THEN \'referral\'
+                    WHEN
+                        ((LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%sign%\')
+                            OR (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%signature%\'))
+                    THEN
+                        \'signature\'
+                    ELSE \'misc\'
+                END) AS `comment_type`
+            FROM
+                `laravel`.`sweetwater_test`'
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared(
+            'DROP VIEW IF EXISTS `comments_report`;'
+        );
+
+        DB::unprepared(
+            'CREATE VIEW `laravel`.`comments_report` AS
+            SELECT 
+                `laravel`.`sweetwater_test`.`orderid` AS `orderid`,
+                `laravel`.`sweetwater_test`.`comments` AS `comments`,
+                `laravel`.`sweetwater_test`.`shipdate_expected` AS `shipdate_expected`,
+                (CASE
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) REGEXP \'(candy|smarties|bit o honey)\') THEN \'candy\'
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) REGEXP \'(call|contact me)\') THEN \'callme\'
+                    WHEN (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%refer%\') THEN \'referral\'
+                    WHEN
+                        ((LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%sign%\')
+                            OR (LOWER(`laravel`.`sweetwater_test`.`comments`) LIKE \'%signature%\'))
+                    THEN
+                        \'signature\'
+                    ELSE \'misc\'
+                END) AS `comment_type`
+            FROM
+                `laravel`.`sweetwater_test`'
+        );
+    }
+};
